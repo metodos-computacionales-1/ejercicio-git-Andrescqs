6933fad988f712d52afd03d0de028d2e88adf6e2
Andrés Camilo Quintero Soto <andrescqs@MacBook-Pro-de-Andres-6.local>
2019-10-29 16:50:33 -0500

    primer

---
+++ test.cpp
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #include <iostream>\
+\
+Int main(void)\
+\{\
+	init N;\
+	std::cout<<\'93diga el numero de elementos a calcular\'94 <<std::end;\
+	std ::cin>>N;\
+	init a = 0;\
+	for(int i=0; i<=N; i++)\{\
+		a+=1;\
+		sed::cout<<std::end;\
+	\}\
+	return 0;\
+\}\
+}
