1d5a596bc1375d25629768ee41233fa220910830
Andrés Camilo Quintero Soto <andrescqs@MacBook-Pro-de-Andres-7.local>
2019-10-30 23:35:00 -0500

    Primer Intento

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+	float calcular([float r]){
+		float d;
+		d = 2*r;
+		float p;
+		p = 2*3.1415926*r;
+		float a;
+		a = 3.1415926*r*r;
+	std::cout << d << " " << p << " " << a << std::endl
+	}
+return 0;
+}
