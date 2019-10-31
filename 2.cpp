e2e4984d22da0007b1367a8a1c32c04eb97ec0e3
Andrés Camilo Quintero Soto <andrescqs@MacBook-Pro-de-Andres-7.local>
2019-10-31 00:54:15 -0500

    prueba 2.2

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-string tri(int a, int b, int c){
-	if(a*a + b*b == c*c){return "Verdad"}
-	else if(c*c - a*a == b*b){return "Verdad"}
-	else if(c*c - b*b == a*a){return "Verdad"}
-	else(){return "Falso"}
+bool tri(int a, int b, int c){
+	return(a*a + b*b == c*c);
 	}
-
 int main(){
 	std::cout <<"Para(3,4,5): " << tri(3,4,5) << ", Para(3,4,6):  " << tri(3,4,6) << std::endl;
 
