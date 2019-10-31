f6af83730bae3f0d8ada7acf99efa712dc8ecea7
Andrés Camilo Quintero Soto <andrescqs@MacBook-Pro-de-Andres-7.local>
2019-10-31 00:22:30 -0500

    quinto

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cmath>
 
+
+float diam(float r){
+	return 2*r;
+	}
+float peri(float r){
+	return 2*3.1415926*r;
+	};
+float area(float r){
+	return 3.1415926*r*r;
+	}
+
 int main(){
-	float diam(float r){
-		return 2*r;
-		}
-	float peri(float r){
-		return 2*3.1415926*r;
-		};
-	float area(float r){
-		return 3.1415926*r*r;
-		}
 	std::cout << diam(2) << " " << peri(2) << " " << area(2) << std::endl;
-	
-return 0;
+	return 0;
 }
